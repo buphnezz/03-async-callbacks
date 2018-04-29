f512e1cecab301178a208f0471e181c199e3e645
zachary schumpert <buphnezz@gmail.com>
2018-04-28 19:54:01 -0700

    upated routes so code functions properly, updated README.md. lab 03 done!

--- lab-zachary/lib/logger.js
+++ lab-zachary/lib/logger.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const winston = require('winston');
-console.log(winston);
+
 const logger = module.exports = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'captains.log', level: 'verbose' }),
+    new winston.transports.File({ filename: 'log.log', level: 'verbose' }),
     new winston.transports.Console({ format: winston.format.simple(), level: 'info' }),
   ],
 });
